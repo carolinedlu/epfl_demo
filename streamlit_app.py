979cf35230666f4769ad13f4cc62e136873bb030
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-07-22 08:56:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ from PIL import Image
 
 st.balloons()
 st.markdown('**Hello EPFL!**')
-image = Image.open('EPFL.png')
-st.image(image)
+# image = Image.open('EPFL.png')
+# st.image(image)
 st.markdown('We are very excited to be working with you.')
