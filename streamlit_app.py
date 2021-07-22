7a5659044b44e8348c0e3d9dc2e84fd89e58a9f5
Caroline Lu <42614552+carolinedlu@users.noreply.github.com>
2021-07-22 08:30:51 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from PIL import Image
+
+st.balloons()
+st.markdown('**Hello EPFL!**')
+image = Image.open('EPFL.png')
+st.image(image)
+st.markdown('We are very excited to be working with you.')
